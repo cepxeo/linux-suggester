9024cd7aeff8e4c596e1135010edd084c6e2c294
cepxeo <you@example.com>
2021-04-11 22:41:45 +0200

    update

--- suggester.sh
+++ suggester.sh
@@ -2,10 +2,8 @@
 host=YOUR_IP
 user=username
 key=id_rsa
-ssh -i $key $user@$host "rm /tmp/linux-*"
 scp -i $key linux-suggester.py $user@$host:/tmp/
-ssh -i $key $user@$host "chmod +x /tmp/linux-suggester.py"
-ssh -i $key $user@$host "python /tmp/linux-suggester.py > /tmp/linux-suggester.log"
+ssh -i $key $user@$host "chmod +x /tmp/linux-suggester.py && python /tmp/linux-suggester.py > /tmp/linux-suggester.log
 mkdir log
 scp -i $key $user@$host:/tmp/linux-*.log log/
 ssh -i $key $user@$host "rm /tmp/linux-*"
